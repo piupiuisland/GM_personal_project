276b9cf96d06a31f4c7bf07567492317a6b52dbc
ruhuizhao <zhaodoudou233666@gmail.com>
2019-03-09 17:23:28 +0100

    3.9 read data

---
+++ read_data.py
@@ -0,0 +1,58 @@
+import csv
+
+###-------------- first part: use csv load and save data -----------------------------
+# read data
+def read_data(file_name,lines):
+    file_path = 'data_player/'
+    file = file_path + file_name
+    needed_data = []
+    with open(file,'r') as csvfile:
+        r = csv.reader(csvfile,delimiter=',')
+        # for row in r:
+        for row in r:
+            needed_data.append(row)
+            if len(needed_data) == lines:      # 一旦行数到了lines这个行数，就停止了，不接着写入了
+                break
+    return needed_data
+
+# save data
+def save_data(data,file_name):
+    file_path = 'data_player/'
+    with open(file_path +file_name+'.csv','w') as csvfile:
+        writer = csv.writer(csvfile)
+        for item in data:                           # 把每个子列表写入 csvfile
+            writer.writerow(item)
+    return
+
+file_1 = '2017_fulldata.csv'
+data_2017 = read_data(file_1,152)
+print(data_2017[0])
+print(data_2017[1])
+print(data_2017[151])
+print(len(data_2017[0]))
+print(len(data_2017[1]))
+print(len(data_2017[2]))
+print(len(data_2017))
+# save_data(data_2017,'data_2017')
+
+###-------------- second part: use pandas load and save data -----------------------------
+
+import pandas as pd
+
+all2018_data = pd.read_csv('data_player/2018_fulldata.csv')
+data_2018 = all2018_data.iloc[0:151,:]
+print(data_2018)
+data_2018.to_csv('data_player/data_2018.csv',index=False)      # index = false 是表示不保存index的意思
+
+
+def use_pandas_readdata(filename,lines,savename):
+    path = 'data_player/'
+    filepath = path + filename +'.csv'
+    all_data = pd.read_csv(filepath)
+    needed_data = all_data.iloc[0:lines,:]
+    savepath = path + savename + '.csv'
+    needed_data.to_csv(savepath,index=False)
+    return
+
+use_pandas_readdata('2019_fulldata',151,'data_2019')
+use_pandas_readdata('2017_fulldata',151,'data_2017')
