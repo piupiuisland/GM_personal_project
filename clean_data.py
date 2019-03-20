119ce84a3982cba807cd12ca55557afdd4ac9efb
ruhuizhao <zhaodoudou233666@gmail.com>
2019-03-20 21:50:51 +0100

    3.20 update

--- clean_data.py
+++ clean_data.py
@@ -1,65 +1,19 @@
 import csv
 import numpy as np
 import string
-
-###-------------- first part: use csv load and save data -----------------------------
-# read data  读文件读到 needed_lines这行
-def read_data(file_name, needed_lines):
-    file_path = 'data_player/'
-    file = file_path + file_name
-    needed_data = []
-    with open(file,'r') as csvfile:
-        r = csv.reader(csvfile,delimiter=',')
-        # for row in r:
-        for row in r:
-            needed_data.append(row)
-            if len(needed_data) == needed_lines:  # 一旦行数到了lines这个行数，就停止了，不接着写入了
-                break
-    return needed_data
-
-# save data
-def save_data(data,file_name):
-    file_path = 'data_player/'
-    with open(file_path +file_name+'.csv','w') as csvfile:
-        writer = csv.writer(csvfile)
-        for item in data:                           # 把每个子列表写入 csvfile
-            writer.writerow(item)
-    return
-
-'''
-file_1 = '2017_fulldata.csv'
-data_2017 = read_data(file_1,401)
-print(data_2017[0])                     # 看第一行
-print(data_2017[1])
-print(data_2017[400])
-print(len(data_2017[0]))
-print(len(data_2017[1]))
-print(len(data_2017[2]))                 # 看数据有多少列
-print(len(data_2017))
-# save_data(data_2017,'data_2017')       # 保存文件
-'''
-
-file_2 = '2019_fulldata.csv'  # initial dataset
-data_2019 = read_data(file_2,21)
-print(data_2019[0])
-print(len(data_2019[0]))
-
-file_1 = '2017_fulldata.csv'
-data_2017 = read_data(file_1,51)
-save_data(data_2017,'2017dat')
-
-
-
-###-------------- second part: use pandas load and save data -----------------------------
-
 import pandas as pd
 
-'''
-all2018_data = pd.read_csv('data_player/2018_fulldata.csv')
-data_2018 = all2018_data.iloc[0:451,:]
+# all_2019data = pd.read_csv('data_player/2018_fulldata.csv')
+# data_2018 = all2018_data.iloc[0:451,:]                   # 读到第451行，下面隔一行保存
 # print(data_2018)
 # data_2018.to_csv('data_player/data_2018.csv',index=False)      # index = false 是表示不保存index的意思
-'''
+
+##############################################################################
+##############################################################################
+
+
+##############################################################################
+###----------------  part 3: 自己写的function 读数据并保存 ----------------------
 
 import pandas as pd
 ### 下面这个function是我自己写的  read 并 save， lines是变量第 lines行
@@ -73,66 +27,34 @@ def use_pandas_readdata(filename,num_ofLines,savename):
     return
 
 use_pandas_readdata('2019_fulldata',451,'data_2019')
-use_pandas_readdata('2017_fulldata',451,'data_2017')
 
 data_2019 = pd.read_csv('data_player/data_2019.csv')
-print(data_2019.columns.tolist())
+print(data_2019.columns.tolist())  # 查看column 的名字，放一个列表里
 
-# print(data_2019.head(20))
-# print(data_2019['Club Logo'])
-
+# 查看这个叫 'Club logo'的这列
 ### 这一行这个 club logo 名字不可用，需要加一个下划线，不然rdf 没法识别
 data_2019.rename(columns={'Club Logo':'Club_Logo'},inplace = True)
+data_2019.to_csv('data_player/data_2019.csv', index=False)
 
-# print(data_2019['Club_Logo'])        # check whether the 'Club Logo' has been changed
-# data_2019.to_csv('data_player/data_2019.csv',index=False)
-# print(data_2019['RCM'])
+##############################################################################
+##############################################################################
+
 
 ########################################################################
-################### work at 3.19  ######################################
-
-#### 下面3句是一起的，但是这个 pd.to_numeric 这个行不通，弄完整个列都是0。。
-
-# print(pd.info(data_2019['LWB']))
-# xx = pd.to_numeric(data_2019['LWB'], errors='coerce').fillna(0)
-# print(xx)
-
-# print(data_2019.info(data_2019['LWB']))
-# data_2019['LWB'].astype(str).astype(int)   # 这个行不通
-
-#### ------------- 上面这5行行不通。。。
-
-# print(data_2019['LWB'])
-# def convert_ability(value):
-#     # new_value = value.repalce('+','')
-#     new_value = value.replace(eval(value))
-#     return np.float(new_value)
-# data_2019['LWB'].apply(convert_ability)
-# list = data_2019['LWB'].tolist()
-# print(list)
-
-
-# def value_convert(column_name):
-#     data_list  = data_2019[column_name].tolist()
-#
-#     for j in data_list:
-#         print(type(j))
-#     return data_list
-# # print(data_list)
-# l = value_convert('LWB')
-
+################### work at 3.16  ######################################
 
 #######   下面这一段代码是清洗列数据
+'''
+####  除掉这个'LWB' columan 中的nan，用0代替
+data_2019 = pd.read_csv('data_player/data_2019.csv')
 
-dd = data_2019['LWB'].fillna(0)      # 除掉这个'LWB' columan 中的nan，用0代替
-print(dd)
-
+dd = data_2019['LWB'].fillna(0)      
 dd_list  = dd.tolist()
 def covert_v(item):                     # 对于输入的item 转换成数字
     x = str(item)
     return eval(x)
 ll = list(map(covert_v,dd_list))       # 把convert function 和
-data_2019['LWB'] = pd.Series(ll)
+data_2019['LWB'] = pd.Series(ll)       # 'LWB'这一列等于上买呢修改过
 data_2019.to_csv('data_player/data_2019_cleaning.csv', index=True)
 clean_data = pd.read_csv('data_player/data_2019_cleaning.csv')
 print(clean_data['LWB'])
@@ -140,10 +62,8 @@ print(clean_data['LWB'])
 ###############
 
 
-## build a function to clean the needed list
-column_list = ['LS','ST','RS','LW','LF']  # 这个list中存着需要clean的 column名
-path = 'data_player/'
 
+path = 'data_player/'
 def covert_v(item):                     # 对于输入的item 转换成数字
     x = str(item)
     return eval(x)
@@ -158,16 +78,21 @@ def clean_columns(filename,name_list):
     data.to_csv(path + filename + '.csv')
     return
 
+
+## build a function to clean the needed list
+
 '''
-clean_columns('data_2019',column_list)
+column_list = ['LS','ST','RS','LW','LF']  # 这个list中存着需要clean的 column名
+clean_columns('data_2019',column_list)    # 测试一下这function
 data_20199 = pd.read_csv(path + 'data_2019' + '.csv')
 print(data_20199[['LS','ST','RS','LW','LF']])
 # print(data_20199['LS'])
 # print(type(data_20199))
-
-
+'''
+## start cleaning
 cols_list = data_2019.columns.tolist()  # column names list
 needs_cols_list = cols_list[28:-1]
 clean_columns('data_2019', needs_cols_list)
-check_data_2019 = pd.read_csv(path + 'data_2019' + '.csv')
-print(check_data_2019[['LS', 'ST', 'RS', 'LW', 'LF']])
+cleaning_data_2019 = pd.read_csv(path + 'data_2019' + '.csv')
+print(cleaning_data_2019[['LS', 'ST', 'RS', 'LW', 'LF']])
+print(cleaning_data_2019.head(20))
